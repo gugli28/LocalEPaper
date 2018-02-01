93470f343c813e7808c3501704c7696abe66fe42
gugli <princebgt28@gmail.com>
2018-02-01 19:46:17 +0530

    merge the list of pdfs

---
+++ pdf_merger.py
@@ -0,0 +1,39 @@
+from PyPDF2 import PdfFileMerger
+import os
+merger = PdfFileMerger()
+
+
+
+def main():
+	pdfs = ['1.pdf', '2.pdf']
+	FileMerger(pdfs, "/home/gugli/Documents/script_py/Dainik_Jagron/01.pdf")
+
+	print check_valid_pdf('1.pdf')
+
+
+def check_valid_pdf(file_path):
+	
+	# dir_path = os.path.dirname(os.path.realpath(__file__))
+	# file_path = dir_path +"/"+filename
+	merger2 = PdfFileMerger()
+	try:
+		merger2.append(file_path)
+	except Exception as e:
+		print "========GOT ERROR WHILE APPENDING FILE==========", file_path
+		print e
+		return 1
+	return 0	 
+		
+		
+def FileMerger(pdfs, file_path):
+	# dir_path = os.path.dirname(os.path.realpath(__file__))
+	for pdf in pdfs:
+		merger.append(pdf)
+	merger.write(file_path)
+
+	print "CONGRATS !!!! PDF MERGED !!!!"
+	
+
+
+if __name__ == "__main__":
+    main()
