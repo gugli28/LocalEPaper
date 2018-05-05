6e8bbb2ddf20a3c4b578732afc7d2ce210c22d33
gugli <princebgt28@gmail.com>
2018-05-05 15:09:53 +0530

    not much

--- scheduleCron.py
+++ scheduleCron.py
@@ -10,8 +10,8 @@ my_cron = CronTab(user='gugli')
 def main():
 	# remove_chron()
 	# add_chron('cd /home/gugli/Documents/script_py/Dainik_Jagron && /usr/bin/python /home/gugli/Documents/script_py/Dainik_Jagron/Dainik_e_paper.py','e-dainik')
-	add_chron('cd /home/gugli/Documents/script_py/Dainik_Jagron && /usr/bin/python /home/gugli/Documents/script_py/Dainik_Jagron/writeDate.py', 'dateinfo')
-
+	# add_chron('cd /home/gugli/Documents/script_py/Dainik_Jagron && /usr/bin/python /home/gugli/Documents/script_py/Dainik_Jagron/writeDate.py', 'dateinfo')
+	add_chron('"/home/gugli/Downloads/tor-browser_en-US/Browser/start-tor-browser" --detach','TOR')
 def remove_chron():
 	for job in my_cron:
 		my_cron.remove(job)
@@ -23,8 +23,8 @@ def add_chron(cmd,com):
 	my_cron.write()
 
 	#scheduling the job
-	job.minute.every(1) 
-
+	# job.minute.every(1) 
+	job.day.every(1) 
 
 	## MANUALLY UPDATED crontab (`crontab -e`) the frequecy * */2 * * * [ at interval of 2 hrs atarting from 0000]
 
@@ -32,7 +32,8 @@ def add_chron(cmd,com):
 
 	## 0,10,20,30,40,50 */1 * * * cd /home/gugli/Documents/script_py/Dainik_Jagron && /usr/bin/python /home/gugli/Documents/script_py/Dainik_Jagron/Dainik_e_paper.py >>/home/gugli/Documents/script_py/Dainik_Jag$
 
-
+	##check og of scheduled jobs
+	## vi /var/log/syslog
 
 if __name__ == "__main__":
     main()
